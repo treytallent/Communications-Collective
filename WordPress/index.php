6301a7b11b3149da612367ce68d4b0a5311b7911
Trey Tallent <treydentallent@gmail.com>
2024-05-23 20:15:15 +0800

    Initial PHP

---
+++ WordPress/index.php
@@ -0,0 +1,69 @@
+<?php get_header(); ?>
+
+<section class="container-fluid header-bg">
+    <div class="container">
+        <div class="row">
+            <h1>the communication collective at murdoch university</h1>
+            <p class="index-header-subtitle">meet. network. expand.</p>
+            <a href="#"><img class="index-header-social" src="Images/Socials/instagram_white.svg" alt="Instagram icon link to Instagram account" /></a>
+            <a href="#"><img class="index-header-social" src="Images/Socials/facebook_white.svg" alt="Facebook icon link to Facebook account" /></a>
+            <a href="#"><img class="index-header-social" src="Images/Socials/linkedin_white.svg" alt="LinkedIn icon link to LinkedIn account" /></a>
+        </div>
+        <a class="btn row mx-0 my-5">Learn More</a>
+    </div>
+</section>
+
+<!-- About -->
+<section class="about-bg container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left-space">
+                <h2 class="text-pink">about the collective</h2>
+                <p class="section-pull-quote">meet. network. expand.</p>
+                <p class="body">
+                    Murdoch University’s First Student Communication Chapter.<br /><br />A
+                    place for students to connect with fellow communication and
+                    media students, creating a support network that will help
+                    you through your time at Murdoch!
+                </p>
+                <a class="btn row mx-0 my-5">Learn More</a>
+            </div>
+            <div class="col-md-6">
+                <div class="arrow-container-left-pink right-space">
+                    <img src="Images/placeholder_image.webp" alt="#" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Opportunities -->
+<section class="opportunities-bg container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="arrow-container-right-orange1 left-space">
+                    <img src="Images/placeholder_image.webp" alt="#" />
+                </div>
+            </div>
+            <div class="col-md-6 right-space">
+                <h2 class="text-orange1 d-flex justify-content-end">
+                    our opportunities
+                </h2>
+                <p class="section-pull-quote d-flex justify-content-end">
+                    meet. network. expand.
+                </p>
+                <p class="body">
+                    Volunteering can be a great way to expand your network and
+                    develop skills. It also looks great on a CV!<br /><br />We
+                    post updates regularly on our social media pages with the
+                    latest and greatest internship and work opportunities across
+                    a range of industries and roles.
+                </p>
+                <a class="btn row mx-0 my-5">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
---
+++ WordPress/opportunities-page-template.php
@@ -0,0 +1,263 @@
+<?php
+/*
+Template Name: Opportunities
+*/
+?>
+<?php get_header(); ?>
+<header class="sm-header header-opportunities">
+    <div class="container">
+        <div class="row align-content-end test">
+            <h1 class="H1-text mb-sm">Opportunities</h1>
+            <p class="mb-sm">
+                Joining The Communications Collective provides you with
+                friendship and exchange with like-minded peers and internship
+                opportunities. Check out if there are any openings right now
+                and read about the experience other members had in the past.
+            </p>
+        </div>
+    </div>
+</header>
+
+<main class="about-main container">
+    <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <!-- Available Opportunities -->
+
+            <section class="row">
+                <div class="col-md-6 order-1">
+                    <h2 class="H2-text mb-sm">Available Opportunities</h2>
+                    <p class="mb-sm">
+                        Keep up to date with our Facebook and Instagram pages
+                        where we post updates with the latest and greatest
+                        internship and work opportunities across a range of
+                        industries and roles.
+                    </p>
+                    <div class="d-flex justify-content-center">
+                        <a href="https://www.facebook.com/" class="btn px-3 mt-0 btn-sm mr-md">Facebook</a>
+                        <a href="https://www.instagram.com/comm.collective" class="btn px-3 mt-0 btn-sm mr-md">Instagram</a>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-xs-4 sm-img d-flex order-media order-2 arrow-container-left-pink align-self-center">
+                    <img src="Images/opportunities_2.webp" class="ar-1 object-fit-cover" alt="Image" />
+                </div>
+            </section>
+
+            <!-- What We Do -->
+            <section>
+                <div class="row">
+                    <div class="col-md-6 offset-md-3 text-center spacing-md">
+                        <h2 class="mb-sm">What We Do</h2>
+                        <h3 class="mb-md">Internship Experiences</h3>
+                        <p class="mb-md">
+                            Members of the Collective have worked on a range of
+                            diverse projects during their internships.
+                        </p>
+                    </div>
+                </div>
+                <div class="row offset-md-1">
+                    <!-- Opportunities Card 1 -->
+                    <div class="card card-pink p-0 col-10 col-lg-5 mb-md">
+                        <div class="order-1 d-flex mt-auto">
+                            <div class="p-sm pr-0">
+                                <h3 class="subheader1-text m-0">Elli Swanson</h3>
+                                <p class="sm-heading m-0">
+                                    BCW Global Copywriter
+                                </p>
+                                <p class="m-0">Strategic Communication</p>
+                            </div>
+                            <a href="https://elliswanson.wixsite.com/portfolio" class="p-sm mt-auto ml-auto accent-link">See More</a>
+                        </div>
+                        <div class="p-0">
+                            <img src="Images/opportunities_3.webp" alt="#" class="ar-1 object-fit-cover mb-sm pos-top w-100 p-0 br-sm" />
+                            <p class="order-0 mb-md mt-auto p-sm">
+                                <span class="H3-text quote-pink">"</span>Working
+                                on behalf of MESH Consultancy, I landed the
+                                opportunity to intern for BCW - Global as a
+                                copywriter. The client, The Rottnest Channel Swim
+                                Association (RSCA), sought a copywriter for their
+                                annual yearbook. In this role I researched and
+                                interviewed four swimmers and created four pieces
+                                of content for their 2021 Yearbook, as well as for
+                                their website and social media.
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Opportunities Card 2 -->
+                    <div class="card card-orange p-0 col-10 col-lg-5 mb-md">
+                        <div class="order-1 d-flex mt-auto">
+                            <div class="p-sm pr-0">
+                                <h3 class="subheader1-text m-0">
+                                    Junita Nathania
+                                </h3>
+                                <p class="sm-heading m-0">
+                                    Perth Blood Institute
+                                </p>
+                                <p class="m-0">
+                                    Strategic Communication and Global Media &
+                                    Communication
+                                </p>
+                            </div>
+                            <a href="https://www.linkedin.com/in/junita-nathania/" class="p-sm mt-auto ml-auto accent-link">See More</a>
+                        </div>
+                        <div class="p-0">
+                            <img src="Images/opportunities_4.webp" alt="#" class="ar-1 object-fit-cover mb-sm pos-top w-100 p-0 br-sm" />
+                            <p class="order-0 mb-md mt-auto p-sm">
+                                <span class="H3-text quote-orange">"</span>
+                                Collaborating with MESH Consultancy. and Perth
+                                Blood Institute, I've developed two online survey
+                                designs for Thrombosis Australia, targeting the
+                                community and healthcare professionals. The
+                                surveys aimed to understand what is effective and
+                                if there are limitations that need to be addressed
+                                with the intended result of improving the
+                                experience, well-being and understanding of the
+                                site's visitors.
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Opportunities Card 3 -->
+                    <div class="card card-yellow p-0 col-10 col-lg-5 mb-md">
+                        <div class="order-1 d-flex mt-auto">
+                            <div class="p-sm pr-0">
+                                <h3 class="subheader1-text m-0">
+                                    Britney Coulson
+                                </h3>
+                                <p class="sm-heading m-0">The West Australian</p>
+                                <p class="m-0">
+                                    Journalism & Global Politics and Party
+                                </p>
+                            </div>
+                            <a href="https://www.linkedin.com/in/britney-coulson-79b4161a0/" class="p-sm mt-auto ml-auto accent-link">See More</a>
+                        </div>
+                        <div class="p-0">
+                            <img src="Images/opportunities_5.webp" alt="#" class="ar-1 object-fit-cover mb-sm pos-top w-100 p-0 br-sm" />
+                            <p class="order-0 mb-md mt-auto p-sm">
+                                <span class="H3-text quote-yellow">"</span>
+                                Britney worked as a writer for The West Australian
+                                as an Intern and scored a full-page spread in The
+                                West's ANZAC Day Edition 2021. The article focuses
+                                on the origins of ANZAC Day in Australia from
+                                ANZAC biscuits to the importance and history of
+                                commemoration on April 25th.
+                            </p>
+                        </div>
+                    </div>
+
+                    <!-- Opportunities Card 4 -->
+                    <div class="card card-pink p-0 col-10 col-lg-5 mb-md">
+                        <div class="order-1 d-flex mt-auto">
+                            <div class="p-sm pr-0">
+                                <h3 class="subheader1-text m-0">
+                                    Jaspreet Sidhu
+                                </h3>
+                                <p class="sm-heading m-0">Radio Fremantle</p>
+                                <p class="m-0">Journalism</p>
+                            </div>
+                            <a href="https://www.linkedin.com/in/jaspreet-sidhu-b78718193/" class="p-sm mt-auto accent-link">See More</a>
+                        </div>
+                        <div class="p-0">
+                            <img src="Images/opportunities_6.webp" alt="#" class="ar-1 object-fit-cover mb-sm pos-right w-100 p-0 br-sm" />
+                            <p class="order-0 mb-md mt-auto p-sm">
+                                <span class="H3-text quote-pink">"</span>
+                                Murdoch Radio's long standing relationship with
+                                Radio Fremantle allowed Jaspreet to develop and
+                                produce radio segments for Radio Fremantle's
+                                drive-time show. In addition to contributing to
+                                the 2 hour long radio segments, she was also the
+                                social media manager for the weekly show.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Career Paths -->
+            <section>
+                <div class="row flex-column ml-0 mb-md">
+                    <h2 class="mb-sm">Career paths</h2>
+                    <h3>What roles you can fill by studying communications</h3>
+                </div>
+                <!-- Carousel -->
+                <div class="row flex-nowrap overflow-auto snap-scroll ml-0">
+                    <!-- Career Card 1 -->
+                    <div class="card card-pink col-8 col-md-5 border-0 p-0">
+                        <h3 class="subheader1-text mb-sm order-1 p-sm py-0 mb-0">
+                            Journalism
+                        </h3>
+                        <p class="order-2 p-sm mb-0">
+                            Through rigorous training and newsroom simulation
+                            students hone critical thinking, communication,
+                            research, media law and digital skills that open a
+                            world of opportunity both in the media industry and
+                            beyond.
+                        </p>
+                        <img class="order-0 about-project-img mb-sm br-sm career-img" src="Images/Careers/journalism.webp" alt="Career" />
+                    </div>
+                    <!-- Career Card 2 -->
+                    <div class="card card-orange col-8 col-md-5 border-0 p-0">
+                        <h3 class="subheader1-text mb-sm order-1 p-sm py-0 mb-0">
+                            Strategic Communication
+                        </h3>
+                        <p class="order-2 p-sm mb-0">
+                            The Bachelor of Communication in Strategic
+                            Communication prepares students for a career
+                            developing, managing, and implementing communication
+                            strategies and campaigns across all sectors and
+                            industries. It is accredited by Public Relations
+                            Institute of Australia (PRIA).
+                        </p>
+                        <img class="order-0 about-project-img mb-sm br-sm career-img" src="Images/Careers/stratcom.webp" alt="Career" />
+                    </div>
+                    <!-- Career Card 3 -->
+                    <div class="card card-yellow col-8 col-md-5 border-0 p-0">
+                        <h3 class="subheader1-text mb-sm order-1 p-sm py-0 mb-0">
+                            Web Communication
+                        </h3>
+                        <p class="order-2 p-sm mb-0">
+                            The Bachelor of Communication in Web Communication
+                            combines an exciting mix of web design, digital
+                            marketing, and public relations.
+                        </p>
+                        <img class="order-0 about-project-img mb-sm br-sm career-img" src="Images/Careers/webcom.webp" alt="Career" />
+                    </div>
+                    <!-- Career Card 4 -->
+                    <div class="card card-pink col-8 col-md-5 border-0 p-0">
+                        <h3 class="subheader1-text mb-sm order-1 p-sm py-0 mb-0">
+                            Master of Communication
+                        </h3>
+                        <p class="order-2 p-sm mb-0">
+                            Gain comprehensive knowledge, practical skills, and
+                            industry exposure in the field of communication. With
+                            its commitment to excellence, industry engagement,
+                            and supportive learning environment, Murdoch
+                            University provides the ideal platform to embark on a
+                            rewarding and successful career in the dynamic world
+                            of communication.
+                        </p>
+                        <img class="order-0 about-project-img mb-sm br-sm career-img pos-left" src="Images/Careers/mcom.webp" alt="Career" />
+                    </div>
+                    <!-- End of carousel -->
+                </div>
+            </section>
+
+            <!-- Join The Collective -->
+            <section class="row">
+                <div class="col-md-6 order-1">
+                    <h2 class="H2-text mb-sm">Join the Collective</h2>
+                    <p class="mb-sm">
+                        Join us as part of the Communications Collective at
+                        Murdoch University to make the most of your degree and
+                        your University experience.
+                    </p>
+                </div>
+                <div class="col-md-6 d-flex order-media order-2 sm-img arrow-container-left-orange2">
+                    <img src="Images/opportunities_999.webp" class="ar-1 object-fit-cover pos-mid" alt="Image" />
+                </div>
+            </section>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
