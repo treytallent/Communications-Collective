7fe01882e47caa548996abd4704f1d00a5ad42ff
Jia-09 <79925230+Jia-09@users.noreply.github.com>
2024-05-28 13:44:03 +0800

    Create content.php

---
+++ WordPress/content.php
@@ -0,0 +1,253 @@
+<?php
+/*
+Template Name: Events
+*/
+?>
+<?php get_header(); 
+?>  
+<div class="container-fluid header header-gradient">
+        <div class="container">
+            <a href="#" class="headerbtn body-text">Featured</a>
+            <h1 class="contentheading">#supporting local businesses</h1>
+            <h4 class="authorname">Jordanreincastle</h4>
+            <p class="header-intro">
+                With the spread of coronavirus around the world, the Australian Government has implemented a new restriction on businesses in the hospitality industry, of strictly take-away and no dine-in. This means many businesses, who make a majority of their income from walk-ins and bookings, have had to close their doors from their primary source of income for the greater good of slowing the virus’ spread.
+            </p>
+            <a href="#" class="headerbtn-1 body-text">Continue Reading</a>
+    
+        </div><!-- container -->
+    </div><!-- containe-fluid -->
+
+<section class="container-fluid content-section">
+<div class="container">
+<h2 class="text-center visitcontent">visit our content</h2>
+
+              <!-- Navbar links -->
+              <nav class="navbar navbar-expand-lg secondary-nav justify-content-front">
+                <div class="container-fluid">
+              <nav class="nav ">
+                <a class="nav-link active" href="#">All post</a>
+                <a class="nav-link" href="#">Featured</a>
+                <a class="nav-link" href="#">Recommendation</a>
+                <a class="nav-link" href="#">News</a>
+                <a class="nav-link" href="#">Study</a>
+            </nav>
+                  <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdownMenuLink"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        recent content
+                    </a>
+                    <div
+                        class="dropdown-menu text-right"
+                        aria-labelledby="navbarDropdownMenuLink"
+                    >
+                        <a class="dropdown-item" href="#">Earliest</a>
+                        <a class="dropdown-item" href="#">Latest</a>
+                        <a class="dropdown-item" href="#">Most Likes</a>
+                </div>
+            </nav>
+
+              <!-- content section  -->
+
+
+              <div class="card-deck">
+                <div class="card card-pink">
+                  <img class="card-img-top" src="images/content/content-image-2.svg" alt="Image">
+                  <a class="content-btn btn-fluid" href="#">News</a>
+                  <div class="card-body">
+                    <p class="content-author">Steph Dawson</p>
+                    <h3 class="article-title">Coronavirus & Mental Health</h3>
+                    <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                    </p>
+                    <a class="continuereading" href="#">Continue Reading</a>
+                  </div>
+                </div>
+                <div class="card card-orange">
+                  <img class="card-img-top" src="images/content/content-image-2.svg." alt="image">
+                  <a class="content-btn btn-fluid" href="#">News</a>
+                  <div class="card-body">
+                    <p class="content-author">Steph Dawson</p>
+                    <h3 class="article-title">Coronavirus & Mental Health</h3>
+                    <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                    </p>
+                    <a class="continuereading" href="#">Continue Reading</a>
+
+                  </div>
+                </div>
+                <div class="card card-blue">
+                  <img class="card-img-top" src="images/content/content-image-2.svg" alt="image">
+                  <a class="content-btn btn-fluid" href="#">News</a>
+                  <div class="card-body">
+                    <p class="content-author">Steph Dawson</p>
+                    <h3 class="article-title">Coronavirus & Mental Health</h3>
+                    <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                    </p>
+                    <a class="continuereading" href="#">Continue Reading</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card-deck">
+              <div class="card card-pink">
+                <img class="card-img-top" src="images/content/content-image-2.svg" alt="Image">
+                <a class="content-btn btn-fluid" href="#">News</a>
+                <div class="card-body">
+                  <p class="content-author">Steph Dawson</p>
+                  <h3 class="article-title">Coronavirus & Mental Health</h3>
+                  <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                  </p>
+                  <a class="continuereading" href="#">Continue Reading</a>
+                </div>
+              </div>
+              <div class="card card-orange">
+                <img class="card-img-top" src="images/content/content-image-2.svg." alt="image">
+                <a class="content-btn btn-fluid" href="#">News</a>
+                <div class="card-body">
+                  <p class="content-author">Steph Dawson</p>
+                  <h3 class="article-title">Coronavirus & Mental Health</h3>
+                  <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                  </p>
+                  <a class="continuereading" href="#">Continue Reading</a>
+
+                </div>
+              </div>
+              <div class="card card-blue">
+                <img class="card-img-top" src="images/content/content-image-2.svg" alt="image">
+                <a class="content-btn btn-fluid" href="#">News</a>
+                <div class="card-body">
+                  <p class="content-author">Steph Dawson</p>
+                  <h3 class="article-title">Coronavirus & Mental Health</h3>
+                  <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                  </p>
+                  <a class="continuereading" href="#">Continue Reading</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="card-deck">
+                <div class="card card-pink">
+                  <img class="card-img-top" src="images/content/content-image-2.svg" alt="Image">
+                  <a class="content-btn btn-fluid" href="#">News</a>
+                  <div class="card-body">
+                    <p class="content-author">Steph Dawson</p>
+                    <h3 class="article-title">Coronavirus & Mental Health</h3>
+                    <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                    </p>
+                    <a class="continuereading" href="#">Continue Reading</a>
+                  </div>
+                </div>
+                <div class="card card-orange">
+                  <img class="card-img-top" src="images/content/content-image-2.svg." alt="image">
+                  <a class="content-btn btn-fluid" href="#">News</a>
+                  <div class="card-body">
+                    <p class="content-author">Steph Dawson</p>
+                    <h3 class="article-title">Coronavirus & Mental Health</h3>
+                    <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                    </p>
+                    <a class="continuereading" href="#">Continue Reading</a>
+  
+                  </div>
+                </div>
+                <div class="card card-blue">
+                  <img class="card-img-top" src="images/content/content-image-2.svg" alt="image">
+                  <a class="content-btn btn-fluid" href="#">News</a>
+                  <div class="card-body">
+                    <p class="content-author">Steph Dawson</p>
+                    <h3 class="article-title">Coronavirus & Mental Health</h3>
+                    <p class="body-text short-para">With the world in a state of emergency due to the coronavirus pandemic, journalists across the world have been releasing stories to keep everyone updated on the crisis. While it is important to follow the stories ...
+                    </p>
+                    <a class="continuereading" href="#">Continue Reading</a>
+                  </div>
+                </div>
+              </div>
+
+<nav aria-label="Page navigation">
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+          <span class="sr-only">Previous</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+
+</section>
+
+<section class="container-fluid make-post postbg">
+<div class="container">
+    <h3 class="text-center postcontent">post your content</h3>
+    <p class="text-center section-pull-quote">"Now! It's your turn to post a blog!"</p>
+    <p class="text-center post-content ">Find what’s you interested and post it to us through the email provided below:
+        murdoch.commcollective@gmail.com
+       </p>
+    
+</div>
+</section>
+
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="index-footer-social col-md-5">
+                <a href="#"
+                    ><img
+                        class="index-footer-social-icon"
+                        src="Images/Socials/instagram_white.svg"
+                        alt="Instagram icon link to Instagram account"
+                /></a>
+                <a href="#"
+                    ><img
+                        class="index-footer-social-icon"
+                        src="Images/Socials/facebook_white.svg"
+                        alt="Facebook icon link to Facebook account"
+                /></a>
+                <a href="#"
+                    ><img
+                        class="index-footer-social-icon"
+                        src="Images/Socials/linkedin_white.svg"
+                        alt="LinkedIn icon link to LinkedIn account"
+                /></a>
+            </div>
+            <div class="col-md-2 footer-logo">
+                <a href="/index.html"
+                    ><img
+                        src="Images/ccmu_logo.svg"
+                        alt="Communication Collective at Murdoch University Logo"
+                /></a>
+            </div>
+            <div class="col-md-5">
+                <p class="body-text align-right">
+                    Say hi! Write us an email:
+                </p>
+                <a
+                    class="body-text align-right"
+                    href="murdoch.commcollective@gmail.com"
+                    >murdoch.commcollective@gmail.com</a
+                >
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
