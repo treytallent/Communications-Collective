72a2f2003116e5566abde2faf0dd864041711786
Jia-09 <79925230+Jia-09@users.noreply.github.com>
2024-06-01 23:16:40 +0800

    Update single.php

--- WordPress/single.php
+++ WordPress/single.php
@@ -13,52 +13,28 @@
         <div class="postcontent">
             <?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
             <?php the_content(); ?>
-        </div><!--postcomtet-->
-                        
-        <div class="content-band">
-            <span class="postmeta-category"><?php the_category(', '); ?></span>
-            <span class="postmeta-comments"><?php comments_popup_link('0 Comments', '1 Comment', '% Comments'); ?></span>
-            
-        </div><!--content-band-->
-    </div><!--post class-->
-    <div class="related-template">
-    <h3>RELATED POSTS</h3>
-    <ul class="rel-list">
-        <?php
-        $backup = $post; //Backup current post object
-        $current = $post->ID;  //get current post id 
-                                
-        global $post;
-
-        //Fetch categories of current post
-        $counter = 0;
-        $allcats = '';
-        foreach ((get_the_category()) as $cat) {
-            if ($counter > 0) $allcats .= ',';
-            $allcats .= $cat->cat_ID;
-            $counter++;
-        }
+        </div><!--postcomtet-->        
         
-    $myposts = get_posts('numberposts=3&order=DESC&category=');
-        foreach ($myposts as $post) :
-            setup_postdata($post);
-            ?>
-        <li>
-            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a>
-            <span class="related-posts-date"><?php the_time('F jS, Y'); ?></span>
-        </li>
-        <?php endforeach;
-                                
-        $post = $backup; //restore current post object
-        wp_reset_query();
-        ?>
-    </ul>
-</div>
+        <!-- // Add the share buttons here -->
+            <div class="share-buttons">
+                <p class="share-this-links text-align-left"></p>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>" target="_blank" class="share-button facebook">
+                    Share on Facebook
+                </a>
+                <a href="https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>&text=<?php echo urlencode(get_the_title()); ?>" target="_blank" class="share-button twitter">
+                    Share on Twitter
+                </a>
+            </div>
 
-    
-    <div class="comments-template">
-            <?php comments_template(); ?>
-        </div>
+
+        <!-- Navigation Links -->
+        <div class="page-navigation">
+        <div class="nav-previous">
+                <?php previous_post_link('%link', '<i class="fas fa-arrow-left"></i> %title', TRUE); ?>
+            </div>
+            <div class="nav-next">
+                <?php next_post_link('%link', '%title <i class="fas fa-arrow-right"></i>', TRUE); ?>
+            </div>
         
     <?php endwhile; ?>
     <?php else: ?>
@@ -69,7 +45,6 @@
 </article>
 </section>
 
-            
      </div>
     </div>
   </div>
