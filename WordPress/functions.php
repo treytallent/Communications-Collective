21b1cdfe1e1d49d8b1eb35f5f44640f35507d35f
Trey Tallent <treydentallent@gmail.com>
2024-06-04 12:59:15 +0800

    Functions: Remove WordPress image dimension override

--- WordPress/functions.php
+++ WordPress/functions.php
@@ -160,4 +160,11 @@ function wpbeginner_numeric_posts_nav($query = null) {
 
 echo '</ul></div>' . "\n";
 }
+
+function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
+    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
+}
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10, 3 );
+
 ?>
