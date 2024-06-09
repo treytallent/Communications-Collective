616724db60fbe0dab1a77d6fc6e7fafd23eaa82d
Schxion <seanchionh0@gmail.com>
2024-06-09 20:10:23 +0800

    changes

--- WordPress/footer.php
+++ WordPress/footer.php
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="row">
             <div class="index-footer-social col-md-5">
-                <a href="#"><img class="index-footer-social-icon" src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/Socials/instagram_white.svg" alt="Instagram icon link to Instagram account" /></a>
-                <a href="#"><img class="index-footer-social-icon" src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/Socials/facebook_white.svg" alt="Facebook icon link to Facebook account" /></a>
-                <a href="#"><img class="index-footer-social-icon" src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/Socials/linkedin_white.svg" alt="LinkedIn icon link to LinkedIn account" /></a>
+                <a href="https://www.instagram.com/comm.collective/"><img class="index-footer-social-icon" src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/Socials/instagram_white.svg" alt="Instagram icon link to Instagram account" /></a>
+                <a href="https://www.facebook.com/commcollective"><img class="index-footer-social-icon" src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/Socials/facebook_white.svg" alt="Facebook icon link to Facebook account" /></a>
+                <a href="https://www.linkedin.com/groups/10440200/"><img class="index-footer-social-icon" src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/Socials/linkedin_white.svg" alt="LinkedIn icon link to LinkedIn account" /></a>
             </div>
             <div class="col-md-2 footer-logo">
-                <a href="/index.html"><img src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/ccmu_logo.svg" alt="Communication Collective at Murdoch University Logo" /></a>
+                <a href="http://170.187.231.66/~mesh20/sub/"><img src="http://170.187.231.66/~mesh20/sub/wp-content/themes/Communications-Collective/Images/ccmu_logo.svg" alt="Communication Collective at Murdoch University Logo" /></a>
             </div>
             <div class="col-md-5">
                 <p class="body-text text-center text-md-right">
